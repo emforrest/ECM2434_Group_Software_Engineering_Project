37c66b26a29cc8c7631cd55c88679aeee1311e32
abih4 <124170960+abih4@users.noreply.github.com>
2024-02-11 15:08:54 +0000

    Update views.py
    Added render() function to link templates

--- sustainabilityGame/user/views.py
+++ sustainabilityGame/user/views.py
@@ -6,10 +6,13 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("This is the user homepage.")
+    #return HttpResponse("This is the user homepage.")
+    return render(request, "user/home.html")
 
 def settings(request):
-    return HttpResponse("This is the user settings page.")
+    #return HttpResponse("This is the user settings page.")
+    return render(request, "user/settings.html")
 
 def upload(request):
-    return HttpResponse("This is the user upload page.")
+    #return HttpResponse("This is the user upload page.")
+    return render(request, "user/upload.html"
