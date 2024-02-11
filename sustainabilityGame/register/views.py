4f73ac51a423b7d69d1a779246b38470ccd15a0e
abih4 <124170960+abih4@users.noreply.github.com>
2024-02-11 15:07:53 +0000

    Update views.py
    Added render() function to link templates

--- sustainabilityGame/register/views.py
+++ sustainabilityGame/register/views.py
@@ -6,4 +6,5 @@ from django.http import HttpResponse
 
 
 def register(request):
-    return HttpResponse("This is the register page.")
+    #return HttpResponse("This is the register page.")
+    return render(request, "register/register.html")
