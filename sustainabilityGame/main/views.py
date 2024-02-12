856fe54e1294be358f6c8100e462d1f55fc82e34
emforrest <69385208+emforrest@users.noreply.github.com>
2024-02-12 12:47:13 +0000

    loaded template

--- sustainabilityGame/main/views.py
+++ sustainabilityGame/main/views.py
@@ -6,6 +6,6 @@ from django.template import loader
 
 def main(request):
     #return HttpResponse("Hello, world. This is main.")
-    #return render(request, "main/main.html")
-    template = loader.get_template("main/main.html")
-    return HttpResponse(template.render(request))
+    return render(request, "main/main.html")
+    #template = loader.get_template("main/main.html")
+    #return HttpResponse(template.render(request))
