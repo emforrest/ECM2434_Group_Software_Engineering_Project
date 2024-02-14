68b98ffa64bb95047998bdef776a314e0332ea4c
giuliabrown <geb221@exeter.ac.uk>
2024-02-14 16:17:25 +0000

    add static dir to settings

--- sustainabilityGame/sustainabilityGame/settings.py
+++ sustainabilityGame/sustainabilityGame/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+        BASE_DIR / "static"
+]
