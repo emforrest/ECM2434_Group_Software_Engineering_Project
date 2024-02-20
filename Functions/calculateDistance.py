c5c2ddade5922a0249005b10cc34b4d2b7cf026f
LAPTOP-31KT6NTB\Samue <sam.e.townley@icloud.com>
2024-02-20 22:07:46 +0000

    Finished function, successfully returns integer value

--- Functions/calculateDistance.py
+++ Functions/calculateDistance.py
@@ -1,9 +1,10 @@
 from travelTypes import TravelType
 import requests
+import json
 
-# API Key: AIzaSyBaD5ewdHFDtDg6gPGxehsW6dUpsKtEBeo
 
 def locationToDistance(origin_lat: float, origin_long: float, dest_lat: float, dest_long: float, transport: TravelType):
+    
     # Create JSON data format of origin and destination latitudes/longitute values required for the request
     data = {"origins": [
                 {"waypoint": {
@@ -38,10 +39,22 @@ def locationToDistance(origin_lat: float, origin_long: float, dest_lat: float, d
         'X-Goog-Api-Key': 'AIzaSyCVEKGGBT_8WryClHT64tPqFONldHdY35Y',
         'X-Goog-FieldMask': 'distanceMeters,condition'
     }
-    
+
+    # Carry out the post request and check if the status code is a success. If not, handle the error accordingly and return 0 to indicate failure
     response = requests.post('https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix', json=data, headers=headers)
-    print(response)
-    print(response.text)
+    if response.status_code != 200:
+        print(f"Error {response.status_code} whilst computing route matrix: {response.reason}")
+        result = json.loads(response.text)
+        print(f"Status: {data[0]['error']['status']}\nMessage: {data[0]['error']['message']}")
+        return 0
+    
+    # Convert text response to JSON and return distance in metres, or 0 if no valid results returned
+    result = json.loads(response.text)
+    if len(result) > 0:
+        if result[0]['condition'] == "ROUTE_EXISTS":
+            return result[0]['distanceMeters']
+    return 0
 
 
-locationToDistance(50.727163, -3.535225, 50.736235, -3.534693, TravelType.CAR)
+# Test function
+print(locationToDistance(50.727163, -3.535225, 50.736235, -3.534693, TravelType.TRAIN))
