8a1403d361c99b76b73225724b68be6462781d7a
LAPTOP-31KT6NTB\Samue <sam.e.townley@icloud.com>
2024-02-20 20:52:27 +0000

    API request is now adjusted base on arguments passed to function

--- Functions/calculateDistance.py
+++ Functions/calculateDistance.py
@@ -3,29 +3,40 @@ import requests
 
 # API Key: AIzaSyBaD5ewdHFDtDg6gPGxehsW6dUpsKtEBeo
 
-def locationToDistance(origin: str, dest: str, transport: TravelType):
+def locationToDistance(origin_lat: float, origin_long: float, dest_lat: float, dest_long: float, transport: TravelType):
+    # Create JSON data format of origin and destination latitudes/longitute values required for the request
     data = {"origins": [
                 {"waypoint": {
                     "location": {
                         "latLng": {
-                            "latitude": 50.727163,
-                            "longitude": -3.535225}
+                            "latitude": origin_lat,
+                            "longitude": origin_long}
                     }
                 }}],
             "destinations": [
                 {"waypoint": {
                     "location": {
                         "latLng": {
-                            "latitude": 50.736235,
-                            "longitude": -3.534693}
+                            "latitude": dest_lat,
+                            "longitude": dest_long}
                     }
-                }}],
-                "travelMode": "DRIVE"}
+                }}]}
     
+    # Add the travel mode to the data based on the TravelType enum value passed in
+    if transport == TravelType.CAR:
+        data['travelMode'] = "DRIVE"
+    elif transport == TravelType.BIKE:
+        data['travelMode'] = "BICYCLE"
+    elif transport == TravelType.WALK:
+        data['travelMode'] = "WALK"
+    else:
+        data['travelMode'] = "TRANSIT"
+    
+    # Define headers needed for the API request. FieldMask indicates what data we would like to be returned.
     headers = {
         'Content-Type': 'application/json',
         'X-Goog-Api-Key': 'AIzaSyCVEKGGBT_8WryClHT64tPqFONldHdY35Y',
-        'X-Goog-FieldMask': 'originIndex,destinationIndex,duration,distanceMeters,status,condition'
+        'X-Goog-FieldMask': 'distanceMeters,condition'
     }
     
     response = requests.post('https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix', json=data, headers=headers)
@@ -33,4 +44,4 @@ def locationToDistance(origin: str, dest: str, transport: TravelType):
     print(response.text)
 
 
-locationToDistance(1,2,3)
+locationToDistance(50.727163, -3.535225, 50.736235, -3.534693, TravelType.CAR)
