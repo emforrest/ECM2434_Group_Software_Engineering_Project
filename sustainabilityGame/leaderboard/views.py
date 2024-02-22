54ad182bb3bbe77cc09cda890c990032ff35f95e
Jack Skinner <jskinner1119@gmail.com>
2024-02-22 22:30:05 +0000

    working on leaderboard

--- sustainabilityGame/leaderboard/views.py
+++ sustainabilityGame/leaderboard/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+    
 
 def leaderboard(request):
+    user_names = ["Sam", "Jack", "Guilia", "Eleanor", "Charles", "Deigo", "Matt", "Solomon", "Achim"]
+    user_co2saved = [15, 12, 11, 10 , 9, 8, 8, 8, 8, 5]
     #return HttpResponse("This is the leaderboard page.")
-    return render(request, "leaderboard/leaderboard.html")
+    return render(request, "leaderboard/leaderboard.html", {'user_names':user_names}, {"user_co2saved":user_co2saved})
