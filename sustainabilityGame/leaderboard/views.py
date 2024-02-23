20fac166e1d3a0450a67f5ff9218873e6a71b8ff
Jack Skinner <jskinner1119@gmail.com>
2024-02-23 00:16:59 +0000

    created the leaderboard page

--- sustainabilityGame/leaderboard/views.py
+++ sustainabilityGame/leaderboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import User
 
 # Render Leaderboard webpages here
 
@@ -7,7 +8,37 @@ from django.http import HttpResponse
     
 
 def leaderboard(request):
-    user_names = ["Sam", "Jack", "Guilia", "Eleanor", "Charles", "Deigo", "Matt", "Solomon", "Achim"]
-    user_co2saved = [15, 12, 11, 10 , 9, 8, 8, 8, 8, 5]
+    user1 = User()
+    user1.name = "Sam"
+    user1.totalCo2Saved = 10.6
+    user1.position = 1
+
+    user2 = User()
+    user2.name = "Abi"
+    user2.totalCo2Saved = 9.6
+    user2.position = 2
+
+    user3 = User()
+    user3.name = "Charles"
+    user3.totalCo2Saved = 8.6
+    user3.position = 3
+
+    user4 = User()
+    user4.name = "Eleanor"
+    user4.totalCo2Saved = 7.4
+    user4.position = 4
+
+    user5 = User()
+    user5.name = "Guilia"
+    user5.totalCo2Saved = 6.5
+    user5.position = 5
+
+    user6 = User()
+    user6.name = "Jack"
+    user6.totalCo2Saved = 5.2
+    user6.position = 6
+
+
+    users = [user1, user2, user3, user4, user5, user6]
     #return HttpResponse("This is the leaderboard page.")
-    return render(request, "leaderboard/leaderboard.html", {'user_names':user_names}, {"user_co2saved":user_co2saved})
+    return render(request, "leaderboard/leaderboard.html", {'users':users})
