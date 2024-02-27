b7e4897ead88304ff419e002a8eec60cc4a6276f
Charles.Symonds.10 <Charles.Symonds.10@gmail.com>
2024-02-27 12:30:46 +0000

    Groups

--- sustainabilityGame/user/views.py
+++ sustainabilityGame/user/views.py
@@ -18,8 +18,9 @@ def home(request):
                          "email": email,
                          "username": username,
                          "myDate": myDate}
-    #return HttpResponse("This is the user homepage.")
+    # return HttpResponse("This is the user homepage.")
     return render(request, "user/home.html", context)
+
 
 @login_required
 def settings(request):
@@ -29,41 +30,43 @@ def settings(request):
     username = request.user.username
     myDate = request.user.date_joined
     context = context = {"firstName": firstName,
-                         "lastName" : lastName,
+                         "lastName": lastName,
                          "email": email,
                          "username": username,
                          "myDate": myDate}
-    #return HttpResponse("This is the user settings page.")
+    # return HttpResponse("This is the user settings page.")
     return render(request, "user/settings.html", context)
+
 
 @login_required
 def upload(request):
     username = request.user.username
     context = context = {"username": username}
-    #return HttpResponse("This is the user upload page.")
+    # return HttpResponse("This is the user upload page.")
     if request.method == "POST":
-        
+
         print(request.POST.get("oncampus"))
         print(request.POST.get("autocomplete"))
         print(request.POST.get("lat"))
         print(request.POST.get("lng"))
         print(request.POST.get("transport"))
-        
+
         origin = getCampusCoords(request.POST.get('oncampus'))
         if origin == {}:
             raise RuntimeError("On-campus location not found!")
-        
+
         transport = convertTravelType(request.POST.get('transport'))
         if transport is None:
             raise RuntimeError("Transport not found!")
-        
-        distance = locationToDistance(origin['latitude'], origin['longitude'], float(request.POST.get('lat')), float(request.POST.get('lng')), transport)
+
+        distance = locationToDistance(origin['latitude'], origin['longitude'], float(request.POST.get('lat')),
+                                      float(request.POST.get('lng')), transport)
         print(distance)
-        
+
         savings = distanceToCO2(distance, transport)
         print(savings)
-        
+
         request.user.profile.total_saving += savings
         request.user.profile.save()
-        
+
     return render(request, "user/upload.html", context)
