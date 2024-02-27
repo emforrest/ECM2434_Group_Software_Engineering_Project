c77d34eb6797ba0b36ac4e5141d55ca662a9315d
SamLolo <sam.e.townley@icloud.com>
2024-02-27 22:04:55 +0000

    Created a locations model inside the main app

--- sustainabilityGame/main/models.py
+++ sustainabilityGame/main/models.py
@@ -4,8 +4,26 @@
 Each Django model is releated to one database table, and can be linked with
 other models. This class defines the datatypes of each attribute (column) 
 and any additonal methods that relate to that table.
+
+Authors:
+- Sam Townley
 """
 
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    """Contains information about a location outside of campus.
+
+    Attributes:
+        id (int): A unique identifier for the location, automatically generated.
+        lat (float): The latitude of the location.
+        lng (float): The longitude of the location.
+        address (string): The full address of the location.
+        postcode (string): The postcode of the location.
+    """
+    id = models.AutoField(primary_key=True, auto_created=True, unique=True)
+    lat = models.FloatField(blank=False)
+    lng = models.FloatField(blank=False)
+    address = models.CharField(max_length=256, blank=False)
+    postcode = models.CharField(max_length=16, blank=False)
