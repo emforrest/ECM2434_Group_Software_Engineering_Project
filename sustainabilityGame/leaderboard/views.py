da019032ad02982c52700d0a37e42f03d120ec13
Jack Skinner <jskinner1119@gmail.com>
2024-02-27 16:42:35 +0000

    worked the leaderboard with the backend

--- sustainabilityGame/leaderboard/views.py
+++ sustainabilityGame/leaderboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import User
+from django.contrib.auth.models import User
+from user.models import Profile
+from .models import Leaderboard_Entry
 
 # Render Leaderboard webpages here
 
@@ -59,6 +61,22 @@ def leaderboard(request):
     user10.position = 19
 
 
-    users = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10]
+    users = []
+    users_profile = Profile.objects.all().order_by("-total_saving")[:5]
+    x = 1
+    for profile in users_profile:
+        user_entry = Leaderboard_Entry()
+        user = profile.user
+        user_entry.name = user.first_name + " " + user.last_name
+        user_entry.totalCo2Saved = profile.total_saving
+        user_entry.position = x 
+        x+= 1
+        users.append(user_entry)
+
+
+        
+    
+
+
     #return HttpResponse("This is the leaderboard page.")
     return render(request, "leaderboard/leaderboard.html", {'users':users})
