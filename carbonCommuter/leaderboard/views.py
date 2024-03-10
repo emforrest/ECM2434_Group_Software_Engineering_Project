cf43dcb12b94603b8044243e079f0cd57b531e56
Jack Skinner <jskinner1119@gmail.com>
2024-03-10 21:52:36 +0000

    worked on updating the code, need to pull models changes.

--- carbonCommuter/leaderboard/views.py
+++ carbonCommuter/leaderboard/views.py
@@ -12,23 +12,24 @@ from django.http import HttpResponse
 def leaderboard(request):
     users = []
     users_journeys = Journey.objects.all().order_by("-user_id")
-    current_id = 1
+    current_id = -1
     for journey in users_journeys:
-        if journey.user_id != current_id: 
+        print(journey.user_id)
+        if journey.user.id != current_id: 
+            if current_id != -1:
+                users.append(user_entry)
             user_entry = Leaderboard_Entry
             current_id = journey.user_id
-            user = Profile.objects.get(user_id = current_id)
-            user_entry.name = user.first_name + " " + user.last_name
-        user_entry.totalCo2Saved += journey.carbon_savings
-        users.append(user_entry)
+            user_entry.name = journey.user.first_name + " " + journey.user.last_name
+            user_entry.totalCo2Saved = 0
+        try:
+            user_entry.totalCo2Saved += journey.carbon_savings
+        except:
+            user_entry.totalCo2Saved += 0
 
     users.sort(key=lambda x: x.totalCo2Saved, reverse=True)
     users = users[:10]
         
-
-        
-    
-
 
     #return HttpResponse("This is the leaderboard page.")
     return render(request, "leaderboard/leaderboard.html", {'users':users})
