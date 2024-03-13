06e04069ec5ed3bac44b69339635bda20ee82a60
SamLolo <sam.e.townley@icloud.com>
2024-03-13 17:59:34 +0000

    Implement Sentry monitoring on production environment

--- carbonCommuter/carbonCommuter/settings/production.py
+++ carbonCommuter/carbonCommuter/settings/production.py
@@ -1,11 +1,12 @@
 import os
+import sentry_sdk
 
 # Basic configuration for the production environment
 # https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 DEBUG = False
-ALLOWED_HOSTS = [".carboncommuter.xyz"]
+ALLOWED_HOSTS = [".carboncommuter.xyz", "127.0.0.1", "localhost"]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -16,7 +17,7 @@ DATABASES = {
         "NAME": "sustainabilitygame",
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
-        "HOST": "127.0.0.1",
+        "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
     }
 }
@@ -27,3 +28,19 @@ DATABASES = {
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+
+# Configure Sentry for error monitoring
+# https://docs.sentry.io/platforms/python/integrations/django/
+
+sentry_sdk.init(
+    dsn = "https://839e41d4294bd406c9960b0167553428@o4506904711069696.ingest.us.sentry.io/4506904722931712",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    enable_tracing = True,
+    traces_sample_rate = 1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate = 1.0,
+)
