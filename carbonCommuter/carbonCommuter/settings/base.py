b6d5ddf7c19d4be8aba1b8fb9cf2d69e29b0d150
LAPTOP-31KT6NTB\Samue <sam.e.townley@icloud.com>
2024-03-14 16:20:13 +0000

    Fix issue with static files

--- carbonCommuter/carbonCommuter/settings/base.py
+++ carbonCommuter/carbonCommuter/settings/base.py
@@ -97,6 +97,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-        BASE_DIR / "static"
-]
