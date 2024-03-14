7907dbee48b1cf78adc4cd89546d9e9b458a7caf
SamLolo <sam.e.townley@icloud.com>
2024-03-14 20:51:01 +0000

    Fix staticfiles settings

--- carbonCommuter/carbonCommuter/settings/base.py
+++ carbonCommuter/carbonCommuter/settings/base.py
@@ -88,9 +88,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '../staticfiles/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '../media/'
 
 
 # Default primary key field type
