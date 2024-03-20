21b3fa6db3700f36069ada88044c20396387d5bc
LAPTOP-31KT6NTB\Samue <sam.e.townley@icloud.com>
2024-03-20 23:22:17 +0000

    Update delete location history SQL

--- database/DeleteLocationsEvent.sql
+++ database/DeleteLocationsEvent.sql
@@ -4,11 +4,7 @@ CREATE EVENT `Every_1_Hour_Cleanup`
 	ON COMPLETION PRESERVE
 DO BEGIN
 	UPDATE user_journey
-    SET origin_id=null
-	WHERE TIMESTAMPDIFF(HOUR, time_finished, now()) > 336 AND NOT(SELECT on_campus FROM main_location WHERE id=origin_id);
-    
-    UPDATE user_journey
-    SET destination_id=null
-	WHERE TIMESTAMPDIFF(HOUR, time_finished, now()) > 336 AND NOT(SELECT on_campus FROM main_location WHERE id=destination_id);
+    SET origin_id=null, destination_id=null
+	WHERE TIMESTAMPDIFF(HOUR, time_finished, now()) > 336;
 END;$$
 DELIMITER ;
